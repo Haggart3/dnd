956bc841464370931bd272210acfd6a96d237209
Haggart3 <haggart3@msu.edu>
2023-04-29 11:03:14 -0400

    APITest

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+
+
+function APItest() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://www.dnd5eapi.co/api/ability-scores/cha')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  },[]);
+
+  return (
+    <div>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre>: 'Loading...'}
+    </div>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <APItest />
       </header>
     </div>
   );
