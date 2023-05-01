4880fc575dd5a02cc3eda658b244a495ce0e7779
Haggart3 <haggart3@msu.edu>
2023-04-30 20:13:31 -0400

    ability-score-changes

--- src/App.js
+++ src/App.js
@@ -3,44 +3,8 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import { Dropdown, DropdownButton } from 'react-bootstrap';
-
-const AbilityScoreInfo = () => {
-  const [ability, setAbility] = useState([]);
-
-  useEffect(() => {
-    fetch('https://www.dnd5eapi.co/api/ability-scores')
-      .then(response => response.json())
-      .then(data => {
-        setAbility(data.results);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [])
-  const handleSelect = (ability) => {
-    fetch(`https://www.dnd5eapi.co/api/ability-scores/${ability.index}`)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          abilityData: data
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-  return (
-    <DropdownButton title = "Select Ability Score">
-      {ability.map(ability => (
-        <Dropdown.Item key={ability.index} onSelect={() => handleSelect(ability)}>
-          {ability.name}
-        </Dropdown.Item>
-      ))}
-    </DropdownButton>
-  );
-}
-
+/*import { Dropdown, DropdownButton } from 'react-bootstrap';*/
+import AbilityScoreInfo from './abilityScore'
 
 
 function APItest() {
@@ -67,7 +31,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <AbilityScoreInfo />
-       <APItest />
+
       </header>
     </div>
   );
---
+++ src/abilityScore.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+
+
+
+const AbilityScoreInfo = () => {
+    const [ability, setAbility] = useState([]);
+    const [abilityInfo, setAbilityInfo] = useState(null);
+    const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      console.log("test")
+      fetch('https://www.dnd5eapi.co/api/ability-scores')
+        .then(response => response.json())
+        .then(data => {
+          setAbility(data.results);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }, [])
+    const handleSelect = (ability) => {
+      setLoading(true);
+      console.log(`Selected ability: ${ability.name}`);
+      fetch(`https://www.dnd5eapi.co/api/ability-scores/${ability.index}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log('Ability Info 2:', data);
+          setAbilityInfo(data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.log(error);
+          setLoading(false);
+        });
+    };
+    console.log("ability:", ability);
+    console.log("abilityInfo:", abilityInfo);
+    return (
+      <div>
+        <DropdownButton title = "Select Ability Score">
+          {ability.map(a => (
+            <Dropdown.Item key={a.index} onClick={() => handleSelect(a)}>
+              {a.name}
+            </Dropdown.Item>
+        ))}
+        </DropdownButton>
+        {loading && <p>Loading...</p>}
+        {abilityInfo && !loading &&
+          <div>
+            <h2>{abilityInfo.full_name}</h2>
+            {abilityInfo.desc.map((desc, index) => (
+            <p key={index}>{desc}</p>
+            ))}
+          </div>
+        }
+      </div>
+    );
+  }
+  
+export default AbilityScoreInfo;
